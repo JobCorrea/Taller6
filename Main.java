1b8436715db40ed799edb1258331be7c0c820e01
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 20:48:48 -0500

    Se prueba la clases creadas - y se sobre escribe datos

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main{
     public static void main(String[] args) {
-        
+        Empleado emp = new Empleado("Carlos", 521454);
+        Gerente gt = new  Gerente("Chuco", 5621487, "Contabilidad");
+
+        emp.mostrarInformacion();
+        gt.mostrarInformacion();
     }
 }

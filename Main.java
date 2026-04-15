635881cd157fdd989bb11c9e0ea620682c737a63
Job Daniel Correa <jobcorre0912@gmail.com>
2026-04-14 21:08:44 -0500

    Ultimas prubas de main OK - y mustra de de entar incorrectamnete en un protected

--- Main.java
+++ Main.java
@@ -5,5 +5,28 @@ public class Main{
 
         emp.mostrarInformacion();
         gt.mostrarInformacion();
+
+        Vehiculo vh = new Vehiculo("Cuatri Moto", "Susuki");
+        Moto mt = new Moto("Sport", "Yahama", 650);
+       
+        // Intento de acceder directamente a atributos protected
+        /*System.out.println(v.tipo);   //  Error de compilación
+        System.out.println(v.marca);  //  Error de compilación
+        System.out.println(m.cilindrada); //  Error de compilación*/
+
+        // Forma correcta: usar los métodos públicos
+        vh.mostarInfo();
+        mt.mostarInfo();
     }
 }
+/*
+error: tipo has protected access in Vehiculo
+System.out.println(v.tipo);
+                       ^
+error: marca has protected access in Vehiculo
+System.out.println(v.marca);
+                       ^
+error: cilindrada has protected access in Moto
+System.out.println(m.cilindrada);
+                       ^
+ */
